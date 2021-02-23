12306376fe58cacd139672ad7396cc3db192c5c4
MayankGupta <mayank.gupta.6.88@gmail.com>
2021-02-23 19:08:00 +0530

    Session One

---
+++ WorkingWithConst.js
@@ -0,0 +1,26 @@
+const name = "Mayank";
+
+// name = "Anshul Gupta";
+
+const age = 20;
+// age = 100;
+
+const employeee = {
+    name: 'Mayank Gupta',
+    age: 20
+}
+
+// employeee = {
+//     name: "Anshul Gupta",
+//     age: 20
+// };
+
+
+employeee.name = "Anshul Gupta";
+
+console.log(employeee.name);
+
+const myArray = [1, 2, 3, 4];
+myArray.push(10);
+myArray = [ 10, 20, 30 ];
+console.log(myArray.length);
